e8cc99e6af1c5061645d51bd4b5062ae3a31d236
Hideki Abe <hideki.abe.0630@gmail.com>
2016-06-03 20:26:58 +0900

    不要なスクリプトを削除

--- chrome/js/content_script.js
+++
@@ -1,7 +0,0 @@
-chrome.extension.onRequest.addListener(
-    function (request, sender, sendResponse) {
-        if (request.method == "getText") {
-            sendResponse({ data: document.all[0].innerHTML, method: "getText" }); //same as innerText
-        }
-    }
-);
